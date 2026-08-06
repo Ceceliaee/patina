2b48d45b030e615d52ebdab7f0febf1d2d523d3e
Zoe <1815842281@qq.com>
2026-08-06 21:27:47 +0800

    refactor(i18n): remove the legacy copy runtime
    Refs #61

--- src/shared/copy/index.ts
+++
@@ -1,3 +0,0 @@
-export { COPY, SUPPORTED_UI_LANGUAGES } from "./bundle.ts";
-export { getUiLocale, getUiText, getUiTextLanguage, setUiTextLanguage, UI_TEXT } from "./runtime.ts";
-export type { UiLanguage, UiText } from "./types.ts";
